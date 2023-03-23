8a6ceef55809d26177dd7f32cc2db6fc7b5a19e9
luis lindonjonson <luizlindonjonson@yahoo.com.br>
2023-03-22 21:37:06 -0300

    Aula 04 - Seletores Simples

---
+++ js/aula3.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    //afeta todos os elementos.
+    //$("*").css("color","#0000FF");
+    //afeta só o id titulo
+    //$("#titulo").css("color","#0000FF");
+    $("a").css("color","red");
+    //afeta uma class
+    $(".exr0").css("color","#0000FF");
+    //afeta por grupo
+    $("h1, a").css("background-color","yellow");
+    });
