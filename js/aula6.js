dea207b77a52970a3e107c8081327975788b29ed
luis lindonjonson <luizlindonjonson@yahoo.com.br>
2023-03-22 22:36:23 -0300

    Aula 06 - Seletores de Ordem

---
+++ js/aula6.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+/*
+//seleciona a ultima linha
+$("tbody tr:last").css("background","pink");
+*/
+/*
+//seleciona todas as linha menos a ultima.
+$("tbody tr:not(tbody tr:last)").css("background","pink");
+*/
+/**
+ *seleciona todas as linha pares 
+$("tbody tr:even").css("background","pink");
+*/
+/*
+*seleciona todas as linha impares 
+$("tbody tr:odd").css("background","pink");
+*/
+/*
+*seleciona determinada linha marcada.
+$("tbody tr:eq(1)").css("background","pink");
+*/
+/*
+*seleciona determinada linha maior que o valor determinado.
+$("tbody tr:gt(1)").css("background","pink");
+*/
+/*
+*seleciona determinada linha menor que o valor determinado.
+$("tbody tr:lt(1)").css("background","pink");
+*/
+
+});
+
+
