f645f4062e0dc2bd2ac923a7dd12c5890cc1b690
luis lindonjonson <luizlindonjonson@yahoo.com.br>
2023-03-24 00:50:11 -0300

    Aula 08 - Seletores de Atributos

---
+++ js/aula8.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    //$("td:contains(Carandiru)").css("background","#CCC");
+    //$("a[name]").css("background","#CCC");
+    //$("a[href^=mailto]").css("background","#CCC");
+    $("a[name!=menu]").css("background","#CCC");
+});
+
+
