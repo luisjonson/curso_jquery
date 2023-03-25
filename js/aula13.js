c754f99dbd741387eefea2cb2ea5ac9e1631ce39
luis lindonjonson <luizlindonjonson@yahoo.com.br>
2023-03-25 00:26:05 -0300

    Aula 13 - Manipulação DOM Inserir na Árvore

---
+++ js/aula13.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("button").click(function(){
+       //$("ul").append("<li>dentro e na ultima posição</li>");
+       //$("ul").prepend("<li>dentro e na primeira posição</li>");
+       //$("ul").before("<h1>Fora e Antes do Elemento.</h1>");
+       //$("ul").after("<h1>Fora e Depois do Elemento.</h1>");
+       //$("ul").wrap("<p></p>");
+       $("ul").append($("li").clone());
+    });
+});
+
+
